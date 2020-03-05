f5c40d194a1e4afdaff9fac7823a72e9547abad5
Dennis Leon <leonde@vmware.com>
2020-03-05 13:38:21 -0800

    Bump dependencies
    - ensure crypt was bumped
    
    [#171612060](https://www.pivotaltracker.com/story/show/171612060)
    
    Co-authored-by: Dennis Leon <leonde@vmware.com>
    Co-authored-by: Julian Hjortshoj <jhjortshoj@pivotal.io>

--- go.mod
+++ go.mod
@@ -3,16 +3,16 @@ module code.cloudfoundry.org/nfsbroker
 go 1.13
 
 require (
-	code.cloudfoundry.org/clock v0.0.0-20180518195852-02e53af36e6c
-	code.cloudfoundry.org/debugserver v0.0.0-20180612203758-a3ba348dfede
-	code.cloudfoundry.org/existingvolumebroker v0.2.0
+	code.cloudfoundry.org/clock v1.0.0
+	code.cloudfoundry.org/debugserver v0.0.0-20200131002057-141d5fa0e064
+	code.cloudfoundry.org/existingvolumebroker v0.6.0
 	code.cloudfoundry.org/goshims v0.1.0
 	code.cloudfoundry.org/lager v2.0.0+incompatible
-	code.cloudfoundry.org/service-broker-store v0.1.0
-	code.cloudfoundry.org/volume-mount-options v0.1.0
+	code.cloudfoundry.org/service-broker-store v0.3.0
+	code.cloudfoundry.org/volume-mount-options v1.1.0
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/onsi/ginkgo v1.12.0
 	github.com/onsi/gomega v1.9.0
-	github.com/pivotal-cf/brokerapi v2.0.5+incompatible
+	github.com/pivotal-cf/brokerapi v6.4.2+incompatible
 	github.com/tedsuo/ifrit v0.0.0-20191009134036-9a97d0632f00
 )
--- vendor/code.cloudfoundry.org/existingvolumebroker/existingvolumebroker.go
+++ vendor/code.cloudfoundry.org/existingvolumebroker/existingvolumebroker.go
@@ -6,6 +6,8 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/pivotal-cf/brokerapi/domain"
+	"github.com/pivotal-cf/brokerapi/domain/apiresponses"
 	"net/http"
 	"path"
 	"regexp"
@@ -19,7 +21,6 @@ import (
 	"code.cloudfoundry.org/service-broker-store/brokerstore"
 	vmo "code.cloudfoundry.org/volume-mount-options"
 	vmou "code.cloudfoundry.org/volume-mount-options/utils"
-	"github.com/pivotal-cf/brokerapi"
 )
 
 const (
@@ -55,7 +56,7 @@ type Broker struct {
 
 //go:generate counterfeiter -o fakes/fake_services.go . Services
 type Services interface {
-	List() []brokerapi.Service
+	List() []domain.Service
 }
 
 func New(
@@ -90,7 +91,8 @@ func (b *Broker) isSMBBroker() bool {
 	return b.brokerType == BrokerTypeSMB
 }
 
-func (b *Broker) Services(_ context.Context) ([]brokerapi.Service, error) {
+
+func (b *Broker) Services(_ context.Context) ([]domain.Service, error) {
 	logger := b.logger.Session("services")
 	logger.Info("start")
 	defer logger.Info("end")
@@ -98,7 +100,7 @@ func (b *Broker) Services(_ context.Context) ([]brokerapi.Service, error) {
 	return b.services.List(), nil
 }
 
-func (b *Broker) Provision(context context.Context, instanceID string, details brokerapi.ProvisionDetails, asyncAllowed bool) (_ brokerapi.ProvisionedServiceSpec, e error) {
+func (b *Broker) Provision(context context.Context, instanceID string, details domain.ProvisionDetails, _ bool) (_ domain.ProvisionedServiceSpec, e error) {
 	logger := b.logger.Session("provision").WithData(lager.Data{"instanceID": instanceID, "details": details})
 	logger.Info("start")
 	defer logger.Info("end")
@@ -108,16 +110,16 @@ func (b *Broker) Provision(context context.Context, instanceID string, details b
 	var decoder = json.NewDecoder(bytes.NewBuffer(details.RawParameters))
 	err := decoder.Decode(&configuration)
 	if err != nil {
-		return brokerapi.ProvisionedServiceSpec{}, brokerapi.ErrRawParamsInvalid
+		return domain.ProvisionedServiceSpec{}, apiresponses.ErrRawParamsInvalid
 	}
 
 	share := stringifyShare(configuration[SHARE_KEY])
 	if share == "" {
-		return brokerapi.ProvisionedServiceSpec{}, errors.New("config requires a \"share\" key")
+		return domain.ProvisionedServiceSpec{}, errors.New("config requires a \"share\" key")
 	}
 
 	if _, ok := configuration[SOURCE_KEY]; ok {
-		return brokerapi.ProvisionedServiceSpec{}, errors.New("create configuration contains the following invalid option: ['" + SOURCE_KEY + "']")
+		return domain.ProvisionedServiceSpec{}, errors.New("create configuration contains the following invalid option: ['" + SOURCE_KEY + "']")
 	}
 
 	if b.isNFSBroker() {
@@ -125,7 +127,7 @@ func (b *Broker) Provision(context context.Context, instanceID string, details b
 		match := re.MatchString(share)
 
 		if match {
-			return brokerapi.ProvisionedServiceSpec{}, errors.New("syntax error for share: no colon allowed after server")
+			return domain.ProvisionedServiceSpec{}, errors.New("syntax error for share: no colon allowed after server")
 		}
 	}
 
@@ -147,20 +149,20 @@ func (b *Broker) Provision(context context.Context, instanceID string, details b
 	}
 
 	if b.instanceConflicts(instanceDetails, instanceID) {
-		return brokerapi.ProvisionedServiceSpec{}, brokerapi.ErrInstanceAlreadyExists
+		return domain.ProvisionedServiceSpec{}, apiresponses.ErrInstanceAlreadyExists
 	}
 
 	err = b.store.CreateInstanceDetails(instanceID, instanceDetails)
 	if err != nil {
-		return brokerapi.ProvisionedServiceSpec{}, fmt.Errorf("failed to store instance details: %s", err.Error())
+		return domain.ProvisionedServiceSpec{}, fmt.Errorf("failed to store instance details: %s", err.Error())
 	}
 
 	logger.Info("service-instance-created", lager.Data{"instanceDetails": instanceDetails})
 
-	return brokerapi.ProvisionedServiceSpec{IsAsync: false}, nil
+	return domain.ProvisionedServiceSpec{IsAsync: false}, nil
 }
 
-func (b *Broker) Deprovision(context context.Context, instanceID string, details brokerapi.DeprovisionDetails, asyncAllowed bool) (_ brokerapi.DeprovisionServiceSpec, e error) {
+func (b *Broker) Deprovision(context context.Context, instanceID string, details domain.DeprovisionDetails, _ bool) (_ domain.DeprovisionServiceSpec, e error) {
 	logger := b.logger.Session("deprovision")
 	logger.Info("start")
 	defer logger.Info("end")
@@ -176,18 +178,18 @@ func (b *Broker) Deprovision(context context.Context, instanceID string, details
 
 	_, err := b.store.RetrieveInstanceDetails(instanceID)
 	if err != nil {
-		return brokerapi.DeprovisionServiceSpec{}, brokerapi.ErrInstanceDoesNotExist
+		return domain.DeprovisionServiceSpec{}, apiresponses.ErrInstanceDoesNotExist
 	}
 
 	err = b.store.DeleteInstanceDetails(instanceID)
 	if err != nil {
-		return brokerapi.DeprovisionServiceSpec{}, err
+		return domain.DeprovisionServiceSpec{}, err
 	}
 
-	return brokerapi.DeprovisionServiceSpec{IsAsync: false, OperationData: "deprovision"}, nil
+	return domain.DeprovisionServiceSpec{IsAsync: false, OperationData: "deprovision"}, nil
 }
 
-func (b *Broker) Bind(context context.Context, instanceID string, bindingID string, bindDetails brokerapi.BindDetails) (_ brokerapi.Binding, e error) {
+func (b *Broker) Bind(context context.Context, instanceID string, bindingID string, bindDetails domain.BindDetails, _ bool) (_ domain.Binding, e error) {
 	logger := b.logger.Session("bind")
 	logger.Info("start", lager.Data{"bindingID": bindingID, "details": bindDetails})
 	defer logger.Info("end")
@@ -204,22 +206,22 @@ func (b *Broker) Bind(context context.Context, instanceID string, bindingID stri
 	logger.Info("starting-broker-bind")
 	instanceDetails, err := b.store.RetrieveInstanceDetails(instanceID)
 	if err != nil {
-		return brokerapi.Binding{}, brokerapi.ErrInstanceDoesNotExist
+		return domain.Binding{}, apiresponses.ErrInstanceDoesNotExist
 	}
 
 	if bindDetails.AppGUID == "" {
-		return brokerapi.Binding{}, brokerapi.ErrAppGuidNotProvided
+		return domain.Binding{}, apiresponses.ErrAppGuidNotProvided
 	}
 
 	opts, err := getFingerprint(instanceDetails.ServiceFingerPrint)
 	if err != nil {
-		return brokerapi.Binding{}, err
+		return domain.Binding{}, err
 	}
 
 	var bindOpts map[string]interface{}
 	if len(bindDetails.RawParameters) > 0 {
 		if err = json.Unmarshal(bindDetails.RawParameters, &bindOpts); err != nil {
-			return brokerapi.Binding{}, err
+			return domain.Binding{}, err
 		}
 	}
 
@@ -228,7 +230,7 @@ func (b *Broker) Bind(context context.Context, instanceID string, bindingID stri
 			if k == disallowed {
 				err := errors.New(fmt.Sprintf("bind configuration contains the following invalid option: ['%s']", k))
 				logger.Error("err-override-not-allowed-in-bind", err, lager.Data{"key": k})
-				return brokerapi.Binding{}, brokerapi.NewFailureResponse(
+				return domain.Binding{}, apiresponses.NewFailureResponse(
 					err, http.StatusBadRequest, "invalid-raw-params",
 				)
 
@@ -240,24 +242,24 @@ func (b *Broker) Bind(context context.Context, instanceID string, bindingID stri
 	mode, err := evaluateMode(opts)
 	if err != nil {
 		logger.Error("error-evaluating-mode", err)
-		return brokerapi.Binding{}, err
+		return domain.Binding{}, err
 	}
 
 	mountOpts, err := vmo.NewMountOpts(opts, b.configMask)
 	if err != nil {
 		logger.Error("error-generating-mount-options", err)
-		return brokerapi.Binding{}, brokerapi.NewFailureResponse(err, http.StatusBadRequest, "invalid-params")
+		return domain.Binding{}, apiresponses.NewFailureResponse(err, http.StatusBadRequest, "invalid-params")
 	}
 
 	if b.bindingConflicts(bindingID, bindDetails) {
-		return brokerapi.Binding{}, brokerapi.ErrBindingAlreadyExists
+		return domain.Binding{}, apiresponses.ErrBindingAlreadyExists
 	}
 
 	logger.Info("retrieved-instance-details", lager.Data{"instanceDetails": instanceDetails})
 
 	err = b.store.CreateBindingDetails(bindingID, bindDetails)
 	if err != nil {
-		return brokerapi.Binding{}, err
+		return domain.Binding{}, err
 	}
 
 	driverName := "smbdriver"
@@ -277,7 +279,7 @@ func (b *Broker) Bind(context context.Context, instanceID string, bindingID stri
 	s, err := b.hash(mountOpts)
 	if err != nil {
 		logger.Error("error-calculating-volume-id", err, lager.Data{"config": mountOpts, "bindingID": bindingID, "instanceID": instanceID})
-		return brokerapi.Binding{}, err
+		return domain.Binding{}, err
 	}
 	volumeId := fmt.Sprintf("%s-%s", instanceID, s)
 
@@ -287,14 +289,14 @@ func (b *Broker) Bind(context context.Context, instanceID string, bindingID stri
 		mountConfig[k] = v
 	}
 
-	ret := brokerapi.Binding{
+	ret := domain.Binding{
 		Credentials: struct{}{}, // if nil, cloud controller chokes on response
-		VolumeMounts: []brokerapi.VolumeMount{{
+		VolumeMounts: []domain.VolumeMount{{
 			ContainerDir: evaluateContainerPath(opts, instanceID),
 			Mode:         mode,
 			Driver:       driverName,
 			DeviceType:   "shared",
-			Device: brokerapi.SharedDevice{
+			Device: domain.SharedDevice{
 				VolumeId:    volumeId,
 				MountConfig: mountConfig,
 			},
@@ -314,7 +316,7 @@ func (b *Broker) hash(mountOpts map[string]interface{}) (string, error) {
 	return fmt.Sprintf("%x", md5.Sum(bytes)), nil
 }
 
-func (b *Broker) Unbind(context context.Context, instanceID string, bindingID string, details brokerapi.UnbindDetails) (e error) {
+func (b *Broker) Unbind(context context.Context, instanceID string, bindingID string, details domain.UnbindDetails, _ bool) (_ domain.UnbindSpec, e error) {
 	logger := b.logger.Session("unbind")
 	logger.Info("start")
 	defer logger.Info("end")
@@ -329,29 +331,29 @@ func (b *Broker) Unbind(context context.Context, instanceID string, bindingID st
 	}()
 
 	if _, err := b.store.RetrieveInstanceDetails(instanceID); err != nil {
-		return brokerapi.ErrInstanceDoesNotExist
+		return domain.UnbindSpec{}, apiresponses.ErrInstanceDoesNotExist
 	}
 
 	if _, err := b.store.RetrieveBindingDetails(bindingID); err != nil {
-		return brokerapi.ErrBindingDoesNotExist
+		return domain.UnbindSpec{}, apiresponses.ErrBindingDoesNotExist
 	}
 
 	if err := b.store.DeleteBindingDetails(bindingID); err != nil {
-		return err
+		return domain.UnbindSpec{}, err
 	}
-	return nil
+	return domain.UnbindSpec{}, nil
 }
 
-func (b *Broker) Update(context context.Context, instanceID string, details brokerapi.UpdateDetails, asyncAllowed bool) (brokerapi.UpdateServiceSpec, error) {
-	return brokerapi.UpdateServiceSpec{},
-		brokerapi.NewFailureResponse(
+func (b *Broker) Update(context context.Context, instanceID string, details domain.UpdateDetails, _ bool) (domain.UpdateServiceSpec, error) {
+	return domain.UpdateServiceSpec{},
+		apiresponses.NewFailureResponse(
 			errors.New("This service does not support instance updates. Please delete your service instance and create a new one with updated configuration."),
 			422,
 			"",
 		)
 }
 
-func (b *Broker) LastOperation(_ context.Context, instanceID string, operationData string) (brokerapi.LastOperation, error) {
+func (b *Broker) LastOperation(_ context.Context, instanceID string, _ domain.PollDetails) (domain.LastOperation, error) {
 	logger := b.logger.Session("last-operation").WithData(lager.Data{"instanceID": instanceID})
 	logger.Info("start")
 	defer logger.Info("end")
@@ -359,17 +361,27 @@ func (b *Broker) LastOperation(_ context.Context, instanceID string, operationDa
 	b.mutex.Lock()
 	defer b.mutex.Unlock()
 
-	switch operationData {
-	default:
-		return brokerapi.LastOperation{}, errors.New("unrecognized operationData")
-	}
+	return domain.LastOperation{}, errors.New("unrecognized operationData")
+}
+
+func (b *Broker) GetInstance(ctx context.Context, instanceID string) (domain.GetInstanceDetailsSpec, error) {
+	panic("implement me")
+}
+
+func (b *Broker) LastBindingOperation(ctx context.Context, instanceID, bindingID string, details domain.PollDetails) (domain.LastOperation, error) {
+	panic("implement me")
+}
+
+
+func (b *Broker) GetBinding(ctx context.Context, instanceID, bindingID string) (domain.GetBindingSpec, error) {
+	panic("implement me")
 }
 
 func (b *Broker) instanceConflicts(details brokerstore.ServiceInstance, instanceID string) bool {
 	return b.store.IsInstanceConflict(instanceID, brokerstore.ServiceInstance(details))
 }
 
-func (b *Broker) bindingConflicts(bindingID string, details brokerapi.BindDetails) bool {
+func (b *Broker) bindingConflicts(bindingID string, details domain.BindDetails) bool {
 	return b.store.IsBindingConflict(bindingID, details)
 }
 
@@ -388,7 +400,7 @@ func evaluateMode(parameters map[string]interface{}) (string, error) {
 			return "r", nil
 		}
 
-		return "", brokerapi.NewFailureResponse(fmt.Errorf("Invalid ro parameter value: %q", roc), http.StatusBadRequest, "invalid-ro-param")
+		return "", apiresponses.NewFailureResponse(fmt.Errorf("Invalid ro parameter value: %q", roc), http.StatusBadRequest, "invalid-ro-param")
 	}
 
 	return "rw", nil
@@ -415,3 +427,4 @@ func stringifyShare(data interface{}) string {
 
 	return ""
 }
+
