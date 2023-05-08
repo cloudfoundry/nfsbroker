cloudfoundry/nfsbroker

commit 35c6a34fbe8326da5006db3515fc8dfec740ecd8

allow specifying uaa creds from env (#178)
the binary currently only accepts uaa creds via cli args. that's bad
because that leaves no option of starting the broker without
potentially leaking creds through the linux audit system.

The code already allows to set the broker user and pass through ENV
VARS. This adds the parsing of ENV VARS for UAA related creds.

UAA_CLIENT_ID
UAA_CLIENT_SECRET

Since the env parsing happens right after parsing the flags, the
introduced ENV VARS take precedence over the values provided via the
flags.

main.go
@@ -148,6 +148,15 @@ func parseCommandLine() {
 func parseEnvironment() {
 	username, _ = os.LookupEnv("USERNAME")
 	password, _ = os.LookupEnv("PASSWORD")
+	uaaClientSecretString, _ := os.LookupEnv("UAA_CLIENT_SECRET")
+	if uaaClientSecretString != "" {
+		uaaClientSecret = &uaaClientSecretString
+	}
+	uaaClientIDString, _ := os.LookupEnv("UAA_CLIENT_ID")
+	if uaaClientIDString != "" {
+		uaaClientSecret = &uaaClientIDString
+	}
+
 }
 
 func checkParams() {

commit 64379848eab3577646f0053e9fc48eda7a1c8ffb

fix bug introduced by 35c6a34
this was hastily written ( unfortunately by me ) and made a mistake when
assigning values. This obviously should be used in the right variable,
which is not uaaClientSecret but uaaClientID

main.go
@@ -156,7 +156,7 @@ func parseEnvironment() {
 	}
 	uaaClientIDString, _ := os.LookupEnv("UAA_CLIENT_ID")
 	if uaaClientIDString != "" {
-		uaaClientSecret = &uaaClientIDString
+		uaaClientID = &uaaClientIDString
 	}
 
 }